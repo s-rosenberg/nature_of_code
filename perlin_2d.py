1efc64cab6fa11b58fbf3c507d20fbb9ba73c3ef
sr <sr@bb.vision>
2022-02-11 07:34:26 -0300

    perlin_2d.py implementacion con barrido medio rari

--- perlin_2d.py
+++ perlin_2d.py
@@ -1,12 +1,11 @@
 """
 usar perlin en 2d para hacer cosas copadas
 """
-from random import Random
+
 from perlin_noise import PerlinNoise
-import perlin_noise
 import pygame
 from random_walker import PequeñoSer, RandomWalker
-
+# import numpy as np
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
@@ -18,29 +17,51 @@ class PerlinSurface():
         self.height = canvas.get_height()
         self.delta_noise = delta_noise
         self.noise = PerlinNoise()
+        # self.surface = np.zeros((self.width,self.height))
+        # print(self.surface)
+        self.all_pixels = []
 
-    def populate_surface(self):
+    def populate_surface(self, fast=False):
         for x in range(0, self.width):
             for y in range(0, self.height):
                 noise = self.noise([x*self.delta_noise, y*self.delta_noise])
                 
                 bright = int(RandomWalker.map_range(noise,-1,1,0,255))
-                # print(noise, bright)
                 color = (bright, bright, bright)
                 pequeño_ser = PequeñoSer(x,y,self.canvas,color,True)
-                # TODO crear matriz o algo y displayear todo
-    
+                if fast:
+                    # updatear de a cachitos la patanalla (ir barriendo tipo pantalla tele)
+                    # definir rect al rededor de pequeño ser
+                    # pygame.display.update(rect)
+                    pequeño_ser.draw(pequeño_ser.x, pequeño_ser.y)
+                    pygame.display.update(x,y,1,1)
+                else:
+                    # implementacion para show surface / no muy eficiente
+                    
+                    self.all_pixels.append(pequeño_ser)
+                
+                
+    def show_surface(self):
+        # esta implementacion es super lenta, necesita tener todos los pixels para updatear
+        for pequeño_ser in self.all_pixels:
+            pequeño_ser.draw(pequeño_ser.x, pequeño_ser.y)
+
+
 if __name__ == '__main__':
     # TODO una clase de canvas
     pygame.init()
     SIZE = 800, 600
     screen = pygame.display.set_mode(SIZE)
     run = True
+    perlin_surface = PerlinSurface(screen,0.1)
+    perlin_surface.populate_surface(fast=True)
+    # perlin_surface.show_surface()
+    # pygame.display.flip()
     while run:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT: run = False
-        perlin_surface = PerlinSurface(screen)
-        perlin_surface.populate_surface()
-        pygame.display.flip()
+        
+        
+        
     pygame.quit()
