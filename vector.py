fd62d2817ab2ea610bc5e05794a8113d75501662
sr <sr@bb.vision>
2022-03-05 09:08:50 -0300

    vector.py cambios en metodos

--- vector.py
+++ vector.py
@@ -1,8 +1,10 @@
 
+import math
 from random_walker import Pixel
 from canvas import Canvas
 import pygame
 import random
+from copy import copy
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -13,14 +15,76 @@ class Vector:
         self.x = x
         self.y = y
     
-    def __add__(self, other_v):
-        x_new = self.x + other_v.x
-        y_new = self.y + other_v.y
+    def __suma(self, other_v):
+        self.x += other_v.x
+        self.y += other_v.y
 
-        return Vector(x_new, y_new)
+    def __add__(self, other_v):
+        new_vector = copy(self)
+        new_vector.__suma(other_v)
+
+        return new_vector
+
+    def __resta(self, other_v):
+        self.x -= other_v.x
+        self.y += other_v.y
+
+    def __sub__(self, other_v):
+        new_vector = copy(self)
+        new_vector.__resta(other_v)
+
+        return new_vector
+
+    def __multiplicacion(self, obj):
+        # obj: int or float
+        self.x *= obj
+        self.y *= obj
+
+    def __mul__(self, obj):
+        if type(obj) == Vector:
+            return self.vector_mul(self, obj) 
+        elif type(obj) in (int, float):
+            return self.scalar_mul(self, obj)
+    
+    def __division(self, obj):
+        if obj != 0:
+            self.__multiplicacion(obj ** -1)
+        
+    def __truediv__(self, obj):
+        if type(obj) in (int, float):
+            new_vector = copy(self)
+            new_vector.__division(obj)
+
+            return new_vector
+
+    def mag(self):
+        # magnitud del vector
+        return math.sqrt(self.x**2 + self.y**2)
+    
+    def normalized(self):
+        new_vector = copy(self)
+        new_vector.normalize()
+
+        return new_vector
+
+    def normalize(self):
+        # convierte a vector unitario
+        mag = self.mag()
+        self.__division(mag)
+
+    @staticmethod
+    def vector_mul(vector_1, vector2):
+        raise NotImplementedError
+    
+    @staticmethod
+    def scalar_mul(vector, scalar):
+        new_vector = copy(vector)
+        new_vector.__multiplicacion(scalar)
+
+        return new_vector
 
 class RandomVectorWalker(Pixel):
-    # TODO reescribir RadomWalker para que funcione con Vector
+    
     def __init__(self, posicion:Vector, canvas:pygame.Surface, color=BLACK, pixel=False):
         x = posicion.x
         y = posicion.y
@@ -66,3 +130,4 @@ if __name__ == '__main__':
     canvas.kill_event = pygame.QUIT
     canvas.main()
     del canvas
+
