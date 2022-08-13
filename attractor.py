30cedea9f0c79ddfe89ebff40418b707f0a97f42
sr <sr@bb.vision>
2022-08-13 12:23:14 -0300

    attractor.py ejercicio

--- attractor.py
+++ attractor.py
@@ -1,8 +1,60 @@
 from vector import Vector
 from ser import Ser
 from canvas import Canvas
+from mover import NewtonMover
+import pygame
+
+def constrain(value:float, low:float, high:float):
+    if value > high: return high
+    elif value < low: return low
+    else: return value
+
+G = 0.4
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 class Attractor(Ser):
     
     def __init__(self, canvas: Canvas, position: Vector, mass: float, radius: float = None, height: float = None, width: float = None) -> None:
         super().__init__(canvas, position, mass, radius, height, width)
+
+    def attract(self, mover:NewtonMover) -> Vector:
+
+        force = self.position - mover.position 
+        distance = force.mag()
+        distance = constrain(distance, 5, 25)
+        force.normalize() # direccion
+        strength = (G * self.mass * mover.mass) / (distance * distance)
+        force *= strength
+
+        return force
+
+if __name__ == '__main__':
+    
+    SIZE = 800, 600
+    canvas = Canvas(SIZE)
+    screen = canvas.get_surface()
+    vel_inicial = Vector(1,0)
+    ac_inicial = Vector(0,0)
+    
+    attractor_mass = 10
+    attractor = Attractor(canvas, position=Vector(SIZE[0]/2,SIZE[1]/2),mass=attractor_mass, radius=attractor_mass)
+    mover_position = attractor.position + Vector(0,20)
+    mover = NewtonMover(canvas=canvas, position = mover_position, mass=5, radius=5,velocity=vel_inicial,aceleration=ac_inicial, max_velocity=0.5)
+
+    def loop_function(events):
+        
+        screen.fill(WHITE)
+        
+        attraction = attractor.attract(mover)
+        mover.apply_force(attraction)
+        mover.update(events)
+        mover.display()
+        attractor.display()
+        # mover.check_bordes(damping=0.75)  
+        pygame.display.flip()
+    canvas.loop_function = loop_function
+    
+    canvas.kill_event = pygame.QUIT
+    canvas.main()
+    del canvas
